52c60918b555bccb8dd3a63c5866e7c28f92d17c
cbalbassos <61438012+cbalbassos@users.noreply.github.com>
2020-03-02 00:10:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var cardVisuals = new Array(4);
-let cardSources = ['c-teacher.png', 'c-student.png', 'c-admin.png', 'c-parent.png']
+let cardSources = ['img/c-teacher.png',
+				   'img/c-student.png',
+				   'img/c-admin.png',
+				   'img/c-parent.png']
 
-let fullGame = ['title.png', cardSources, 'great-work.png' ]
+let fullGame = ['img/title.png', cardSources, 'img/great-work.png' ]
+
 
 
 /* SHUFFLE */
 let shuffleCards = function(arr){
-	// idea is to start from the last item in the array
 	let newPosition,
 		temp;
 	for (let i = arr.length - 1; i > 0; i--) {
 		newPosition = Math.floor(Math.random() * (i + 1));
-		//setting each position to a random number
-		// Math.floor to remove decimal
 		temp = arr[i];
 		arr[i] = arr[newPosition];
 		arr[newPosition] = temp;
@@ -27,86 +27,23 @@ let newArray = shuffleCards(fullGame[1]);
 var imageIndex = 0;
 
 function changeImage(){
-	firstImage.setAttribute("src", fullGame[1][imageIndex]);
-	//firstImage.setAttribute("src", newArray[imageIndex]);
-	//firstImage.setAttribute("src", fullGame[imageIndex]);
 	imageIndex++;
-	if (imageIndex > 4) {
-		firstImage.setAttribute("src", fullGame[2]);
-		//imageIndex = 0;
+	
+	if (imageIndex < newArray.length + 1) {
+		firstImage.setAttribute("src", fullGame[1][imageIndex -1]);
+	}
+		
+	else if (imageIndex == newArray.length + 1) {
+		firstImage.setAttribute("src", fullGame[2]); //set firstImage to "great-work card"
 		newArray = shuffleCards(cardSources);
-		// take it out of the array and put it back in again
+	}
+	
+	else if (imageIndex > newArray.length + 1) {
 		location.reload();
 	}
-}
-
-// for every array in the fullGame array
-// if the length of the array > 1
-	// run the changeImage function
-// else
-	// imageIndex
-
-
-
-/*
-// for every card in cardVisuals, link to a card in cardSources
-for (var i = 0; i < cardVisuals.length; i++) {
-	cardVisuals[i] = document.createElement("img");
-	cardVisuals[i].src = cardSources[i];
-	document.body.appendChild(cardVisuals[i]);	
-}
-
-
-
-
-/*
-var testCard = new Image();
-var x = document.getElementById("admin").src;
-x = "img/c-admin.png";
-document.getElementById("myImg").src = "hackanm.gif";
-testCard.src = document.getElementsByClassName("cardOne");
-document.body.appendChild(testCard);
-
-
-CREATE SLIDESHOW 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-
-function showSlides(n) {
-  var i;
-  var slides = newArray;
-  if (n > slides.length) { // when the user reaches the last slide, then reset. but we want to change this so that the "Game over! Restart?" card appears
-	  slideIndex = 1
-  }
+		
 	
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
 }
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
