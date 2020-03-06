bd6f66e4d8bf2b9e3b14f5e101307c549b13a7d1
cbalbassos <61438012+cbalbassos@users.noreply.github.com>
2020-03-06 16:00:16 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let cardSources = ['img/c-teacher.png',
-				   'img/c-student.png',
-				   'img/c-admin.png',
-				   'img/c-parent.png']
+let cardSources = ['img/c-teacher.svg',
+				   'img/c-student.svg',
+				   'img/c-admin.svg',
+				   'img/c-parent.svg']
 
-let fullGame = ['img/title.png', cardSources, 'img/great-work.png' ]
+let fullGame = ['img/title.svg', cardSources, 'img/great-work.svg' ]
 
 
 
@@ -28,22 +28,54 @@ var imageIndex = 0;
 
 function changeImage(){
 	imageIndex++;
+	document.getElementById("count").style.color = "#FFFFFF";
+	
+	
+	if (imageIndex == 1) {
+		timer();
+	}
+	
 	
 	if (imageIndex < newArray.length + 1) {
 		firstImage.setAttribute("src", fullGame[1][imageIndex -1]);
 	}
 		
 	else if (imageIndex == newArray.length + 1) {
+		document.getElementById("count").style.color = "#15364F";
 		firstImage.setAttribute("src", fullGame[2]); //set firstImage to "great-work card"
-		newArray = shuffleCards(cardSources);
+		newArray = shuffleCards(cardSources); //shuffle cards
 	}
 	
-	else if (imageIndex > newArray.length + 1) {
+	else if (imageIndex > newArray.length + 1) { //if the user goes through the entire array
 		location.reload();
-	}
-		
+	}	
 	
 }
+
+
+
+//TIMER
+function timer() {
+	var count = 59;
+	var interval = setInterval
+		(function(){
+		  document.getElementById('count').innerHTML=count;
+		  count--;
+		  if (count === -1){
+			document.getElementById('count').innerHTML="Time's Up";
+			firstImage.setAttribute("src", fullGame[2]); // shows the "great-work" card
+			imageIndex = 5  
+			clearInterval(interval);
+		  }
+		}, 1000);
+}
+
+
+
+
+
+
+
 
 
 
